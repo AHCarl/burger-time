4f32130aa5a869d3b331ab98c7ae718961ac5d29
Robert Schulte <schulte.robertw@outlook.com>
2018-12-31 07:53:54 -0600

    started adding semantic ui cards

--- frontend/src/components/PlaceCard.js
+++ frontend/src/components/PlaceCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class PlaceCard extends React.Component {
     render () {
         return (
-            <p>{this.props.name}</p>
+            <div className="card" >{this.props.name}</div>
         )
     }
 }
--- frontend/src/containers/PlacesContainer.js
+++ frontend/src/containers/PlacesContainer.js
@@ -13,7 +13,7 @@ export default class PlacesContainer extends React.Component {
         return (
             <div style={{ height: '80vh', width: '50%', float: 'left', filter: 'drop-shadow(16px 16px 10px black)' }}>
                 <div>Burger Content based on: {this.props.address}</div>
-                <div>{this.listPlaces(this.props.places)}</div>
+                <div className="ui four cards" >{this.listPlaces(this.props.places)}</div>
             </div>   
         )
     }
