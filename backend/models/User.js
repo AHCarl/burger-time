e4015d4bd9ef3388c7cf9af7eecb1a505bbfd181
AHCarl <ahcarlin@gmail.com>
2019-01-04 09:32:25 -0600

    promise-based API calls

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,8 +18,8 @@ const userSchema = new Schema({
     burgers: [
         { name: String,
           location: Object,
-          distance: Number,
-          time: Number
+        //   distance: Number,
+        //   time: Number
         }
     ]
 })
@@ -51,36 +51,68 @@ bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
 
 const ModelClass = mongoose.model('users', userSchema)
 
-ModelClass.update = (user, newAddress, res) => {
-    googleMapsClient.geocode({address: newAddress}, (err, resp) => {
-        if (!err) {
+// ModelClass.update = (user, newAddress, res) => {
+//     googleMapsClient.geocode({address: newAddress}, (err, resp) => {
+//         if (!err) {
 
-            const location = {
-                address: resp.json.results[0].formatted_address,
-                coords: resp.json.results[0].geometry.location
-            }
+//             const location = {
+//                 address: resp.json.results[0].formatted_address,
+//                 coords: resp.json.results[0].geometry.location
+//             }
             
-            ModelClass.findOneAndUpdate({email: user.email}, {location: location}, (err, user) => {
-                if (err) { 
-                    console.log(err);
-                } else {
-                    user.location = location
-                    !!res && res.status(200).json(user);
-                }
-            })
-        }
+            // ModelClass.findOneAndUpdate({email: user.email}, {location: location}, (err, user) => {
+            //     if (err) { 
+            //         console.log(err);
+            //     } else {
+            //         user.location = location
+            //         !!res && res.status(200).json(user);
+            //     }
+            // })
+//         }
+//     })
+// }
+
+ModelClass.update = (user, newAddress, res) => {
+    googleMapsClient.geocode({address: newAddress}).asPromise()
+    .then((resp) => {
+        const location = {
+                            address: resp.json.results[0].formatted_address,
+                            coords: resp.json.results[0].geometry.location
+                        }
+        ModelClass.findOneAndUpdate({email: user.email}, {location: location}, (err, user) => {
+            if (err) { 
+                console.log(err);
+            } else {
+                user.location = location
+                !!res && res.status(200).json(user);
+            }
+        })
+    })
+    .catch((err) => {
+        console.log(err)
     })
 }
 
 //hardcoded for testing purposes 
-googleMapsClient.placesNearby({location: "29.7590859,-95.3636325",
-type: "restaurant", keyword: "hamburger,burger,burgers", rankby: "distance",
-maxprice: 2, opennow: true}, (err, resp) => {
-    if (!err) {
-        console.log(resp.json)
-    }
-}
-)
-// myBurgers()
+
+
+// googleMapsClient.placesNearby({location: "29.7720111,-95.4125428",
+// type: "restaurant", keyword: "hamburger,burger,burgers", rankby: "distance",
+// maxprice: 2, opennow: true}, (err, resp) => {
+//     if (!err) {
+//         let myPlaces = []
+//         resp.json.results.slice(0,6).forEach(place => {
+//             let burger = {} 
+//             burger.name = place.name 
+//             burger.location = place.geometry.location
+//             myPlaces.push(burger)
+//         })
+//         console.log(myPlaces)
+
+    
+//     }
+// })
+
+
 
 module.exports = ModelClass
