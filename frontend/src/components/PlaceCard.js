0d4eb37a86bfc099f4edbe2bf1ae8c55fc0632ab
Robert Schulte <schulte.robertw@outlook.com>
2019-01-04 16:40:35 -0600

    added burger time

--- frontend/src/components/PlaceCard.js
+++ frontend/src/components/PlaceCard.js
@@ -20,6 +20,9 @@ export default class PlaceCard extends React.Component {
                     <Card.Meta>
                         <span className='price'>Price: {'$$$$'.slice(0, this.props.price_level)} (of 4)</span>
                     </Card.Meta>
+                    <Card.Meta>
+                        <span className='burgerTime'>Burger Time: {this.props.time}</span>
+                    </Card.Meta>
                 </Card.Content>
                 <Card.Content extra>
                     <Button basic color='green' name={this.props.name} onClick={(e) => this.handleClick(e)}>Get Directions</Button>
